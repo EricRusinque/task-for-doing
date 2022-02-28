775e441278df42f92cf38296100c4ab6e912b818
unknown <emprenderusinque@hotmail.com>
2022-02-28 09:39:49 -0500

    done

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 require('colors');
+
+const { showMenu, pause } = require('./helpers/messages')
+
 console.clear();
 
 
 const main = async() => {
-
-
     console.log('Hello World');
+
+    let opt = '';
+
+    do {
+        opt = await showMenu();
+        console.log({ opt })
+        await pause();
+    } while ( opt !== '0');
+
+
+    pause();
 
 }
 
